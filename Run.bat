375cc3ac9abb1b62f636cf9feec882d11bb1f477
Ben Alter <ben.alter@yahoo.com>
2012-06-03 11:18:18 -0400

    Added choices.

--- Run.bat
+++ Run.bat
@@ -19,22 +19,41 @@ if '%errorlevel%' NEQ '0' (
     if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
     pushd "%CD%"
     CD /D "%~dp0"
+
+echo This program will modify your registry/PATH. If you have any questions regarding what this program does specifically, please ask =oxai on deviantART.
+echo.
+:choice
+set /p choice="Do you want to continue? [Y\N\S (skips modifying the registry)] "
+if '%choice%'=='n' goto :quit
+if '%choice%'=='N' goto :quit
+if '%choice%'=='y' goto :continue
+if '%choice%'=='Y' goto :continue
+if '%choice%'=='s' goto :skipreg
+if '%choice%'=='S' goto :skipreg
+if '%choice%'=='skip' goto :skipreg
+echo.
+echo %choice% is not a valid choice.
+goto :choice
+
+:continue
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v "Path" /t REG_EXPAND_SZ /d "%PATH%;%HOMEDRIVE%\php" /f
 cls
-echo Succesfully edited path.
 
+:skipreg
 mkdir temp
 bins\wget -P temp/ http://www.yufexa.com/test/phplist/
 bins\cat temp/index.html > temp/tmpFile
 set /p tmpVar=<temp/tmpFile
+
 bins\wget %tmpVar%
 rd /s /q temp
+
 mkdir php
 bins\7za x -ophp *.zip
 del *.zip
+
 copy config\php.ini php\php.ini
 
-echo Press 'D'
 mkdir %HOMEDRIVE%\php
 xcopy php "%HOMEDRIVE%\php" /S
 rd /s /q php
@@ -46,3 +65,5 @@ echo You will have to restart your computer at your own discretion. Thank you.
 echo.
 echo If you have any questions or concerns, please contact =oxai on deviantART.
 pause
+:quit
+exit
